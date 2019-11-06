31946bb6ccfd9c9f7fa1dbecfc4bbbebc42c5667
Jaehong Yoo <rouddy@naver.com>
2019-11-06 16:07:36 +0900

    sharedpreferenceutil에서 context를 parameter로 넘기게 수정

--- algorigoutil/src/main/java/com/algorigo/library/SharedPreferencesUtil.kt
+++ algorigoutil/src/main/java/com/algorigo/library/SharedPreferencesUtil.kt
@@ -11,78 +11,74 @@ import java.util.*
 object SharedPreferencesUtil {
     private const val STORAGE_KEY = "pref"
     const val SESSION_KEY = "cookies"
-    private var weakReference: WeakReference<Context>? = null
-    fun setContext(context: Context) {
-        weakReference = WeakReference(context)
-    }
 
-    fun setString(key: String, value: String) {
-        val editor = weakReference!!.get()!!.getSharedPreferences(STORAGE_KEY, 0).edit()
+    fun setString(context: Context, key: String, value: String) {
+        val editor = context.getSharedPreferences(STORAGE_KEY, 0).edit()
         editor.putString(key, value)
         editor.apply()
     }
 
-    fun getString(key: String): String {
-        return weakReference!!.get()!!.getSharedPreferences(STORAGE_KEY, 0).getString(key, "")
+    fun getString(context: Context, key: String): String {
+        return context.getSharedPreferences(STORAGE_KEY, 0).getString(key, "")!!
     }
 
-    fun setInt(key: String, value: Int) {
-        val editor = weakReference!!.get()!!.getSharedPreferences(STORAGE_KEY, 0).edit()
+    fun setInt(context: Context, key: String, value: Int) {
+        val editor = context.getSharedPreferences(STORAGE_KEY, 0).edit()
         editor.putInt(key, value)
         editor.apply()
     }
 
-    fun getInt(key: String): Int {
-        return weakReference!!.get()!!.getSharedPreferences(STORAGE_KEY, 0).getInt(key, 0)
+    fun getInt(context: Context, key: String): Int {
+        return context.getSharedPreferences(STORAGE_KEY, 0).getInt(key, 0)
     }
 
-    fun setBoolean(key: String, value: Boolean) {
-        val editor = weakReference!!.get()!!.getSharedPreferences(STORAGE_KEY, 0).edit()
+    fun setBoolean(context: Context, key: String, value: Boolean) {
+        val editor = context.getSharedPreferences(STORAGE_KEY, 0).edit()
         editor.putBoolean(key, value)
         editor.apply()
     }
 
-    fun getBoolean(key: String): Boolean {
-        return weakReference!!.get()!!.getSharedPreferences(STORAGE_KEY, 0).getBoolean(key, false)
+    fun getBoolean(context: Context, key: String): Boolean {
+        return context.getSharedPreferences(STORAGE_KEY, 0).getBoolean(key, false)
     }
 
-    fun setSet(key: String, value: Set<String>) {
-        val editor = weakReference!!.get()!!.getSharedPreferences(STORAGE_KEY, 0).edit()
+    fun setSet(context: Context, key: String, value: Set<String>) {
+        val editor = context.getSharedPreferences(STORAGE_KEY, 0).edit()
         editor.putStringSet(key, value)
         editor.apply()
     }
 
-    fun getSet(key: String): Set<String> {
-        return weakReference!!.get()!!.getSharedPreferences(STORAGE_KEY, 0).getStringSet(key, HashSet())
+    fun getSet(context: Context, key: String): Set<String> {
+        return context.getSharedPreferences(STORAGE_KEY, 0).getStringSet(key, HashSet())!!
     }
 
-    fun setString(prefKey: String, key: String, value: String) {
-        val editor = weakReference!!.get()!!.getSharedPreferences(prefKey, 0).edit()
+    fun setString(context: Context, prefKey: String, key: String, value: String) {
+        val editor = context.getSharedPreferences(prefKey, 0).edit()
         editor.putString(key, value)
         editor.apply()
     }
 
-    fun getString(prefKey: String, key: String): String {
-        return weakReference!!.get()!!.getSharedPreferences(prefKey, 0).getString(key, "")
+    fun getString(context: Context, prefKey: String, key: String): String {
+        return context.getSharedPreferences(prefKey, 0).getString(key, "")!!
     }
 
-    fun setBoolean(prefKey: String, key: String, value: Boolean) {
-        val editor = weakReference!!.get()!!.getSharedPreferences(prefKey, 0).edit()
+    fun setBoolean(context: Context, prefKey: String, key: String, value: Boolean) {
+        val editor = context.getSharedPreferences(prefKey, 0).edit()
         editor.putBoolean(key, value)
         editor.apply()
     }
 
-    fun getBoolean(prefKey: String, key: String): Boolean {
-        return weakReference!!.get()!!.getSharedPreferences(prefKey, 0).getBoolean(key, false)
+    fun getBoolean(context: Context, prefKey: String, key: String): Boolean {
+        return context.getSharedPreferences(prefKey, 0).getBoolean(key, false)
     }
 
-    fun setInt(prefKey: String, key: String, value: Int) {
-        val editor = weakReference!!.get()!!.getSharedPreferences(prefKey, 0).edit()
+    fun setInt(context: Context, prefKey: String, key: String, value: Int) {
+        val editor = context.getSharedPreferences(prefKey, 0).edit()
         editor.putInt(key, value)
         editor.apply()
     }
 
-    fun getInt(prefKey: String, key: String): Int {
-        return weakReference!!.get()!!.getSharedPreferences(prefKey, 0).getInt(key, 0)
+    fun getInt(context: Context, prefKey: String, key: String): Int {
+        return context.getSharedPreferences(prefKey, 0).getInt(key, 0)
     }
 }
