075201bc1567d6cae90077d28251b3259fd29681
seunghwan <slflfl12@naver.com>
2022-07-12 11:24:39 +0900

    RxWifiManager에 checkWifiConnectedObservable 추가

--- algorigoutil/src/main/java/com/algorigo/library/rx/RxWifiManager.kt
+++ algorigoutil/src/main/java/com/algorigo/library/rx/RxWifiManager.kt
@@ -9,6 +9,7 @@ import android.net.wifi.*
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
+import com.jakewharton.rxrelay3.PublishRelay
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
@@ -20,7 +21,7 @@ object RxWifiManager {
     class RemoveWifiOwnerException : Throwable("Wifi is not added by this application")
 
     enum class Connectivity {
-        CONNECTED,
+        CONNECTED, DISCONNECTED
     }
 
     private val LOG_TAG = RxWifiManager::class.java.simpleName
@@ -150,6 +151,64 @@ object RxWifiManager {
         } else {
             Observable.error(NullPointerException())
         }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun checkWifiConnectedObservable(context: Context): Observable<Connectivity> {
+        val relay = PublishRelay.create<Connectivity>()
+        val connectivityManager = context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+
+        val callback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                Log.e("!!!", "onAvailable:$network")
+                relay.accept(Connectivity.CONNECTED)
+            }
+
+            override fun onLosing(network: Network, maxMsToLive: Int) {
+                super.onLosing(network, maxMsToLive)
+                Log.e("!!!", "onLosing:$network")
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                Log.e("!!!", "onLost:$network")
+                relay.accept(Connectivity.DISCONNECTED)
+            }
+
+            override fun onUnavailable() {
+                super.onUnavailable()
+                Log.e("!!!", "onUnavailable")
+            }
+
+            override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
+                super.onCapabilitiesChanged(network, networkCapabilities)
+                Log.e("!!!", "onCapabilitiesChanged:$network, $networkCapabilities")
+            }
+
+            override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
+                super.onLinkPropertiesChanged(network, linkProperties)
+                Log.e("!!!", "onLinkPropertiesChanged:$network, $linkProperties")
+            }
+
+            override fun onBlockedStatusChanged(network: Network, blocked: Boolean) {
+                super.onBlockedStatusChanged(network, blocked)
+                Log.e("!!!", "onBlockedStatusChanged:$network, $blocked")
+            }
+        }
+        val networkRequest = NetworkRequest
+            .Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .build()
+
+        return relay
+            .doOnSubscribe {
+                connectivityManager?.requestNetwork(networkRequest, callback)
+            }
+            .doFinally {
+                connectivityManager?.unregisterNetworkCallback(callback)
+            }
     }
 
     @Suppress("DEPRECATION")
